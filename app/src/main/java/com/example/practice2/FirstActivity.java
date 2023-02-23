2ab9c5084185c697a17996279318fe1ac51b0c76
Svetlana Zharkova <svetazhar7@gmail.com>
2023-02-23 23:17:16 +0300

    Инициализации нескольких компонентов в верстке данными из программного кода

--- app/src/main/java/com/example/practice2/FirstActivity.java
+++ app/src/main/java/com/example/practice2/FirstActivity.java
@@ -3,6 +3,8 @@ package com.example.practice2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_activity);
+        TextView textView = findViewById(R.id.textView9);//textView9 - это фио пользователя
+        String text = getResources().getString(R.string.fio);//инициализация строкой fio
+        textView.setText(text);//установка значения
+
+        ImageView imageView = findViewById(R.id.imageView4);//imageView4 - фото профиля пользователя
+        imageView.setImageResource(R.drawable.ava1);//установка изображения
     }
 }
+
+
